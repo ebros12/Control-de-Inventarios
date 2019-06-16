c68ac05eb64e9172e60991a7fa20ca0d0aa99faa
ebros <ebros121@gmail.com>
2019-06-15 21:49:29 -0500

    Final

--- server/models/countBodega.js
+++ server/models/countBodega.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:server/models/countBodega.js
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -8,25 +7,7 @@ var countBodegaSchema = new Schema({
     nombre: { type: String, required: [true, 'El nombre es necesario'] },
     cantidad: { type: String, required: [true, 'El precio únitario es necesario'] },
     bodega: { type: String, required: [true, 'la bodega es necesaria'] },
-
     valor: { type: String, required: [true, 'el valor es necesariaa'] },
 });
 
-
-=======
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-
-
-var countBodegaSchema = new Schema({
-    nombre: { type: String, required: [true, 'El nombre es necesario'] },
-    cantidad: { type: String, required: [true, 'El precio únitario es necesario'] },
-    bodega: { type: String, required: [true, 'la bodega es necesaria'] },
-
-    valor: { type: String, required: [true, 'el valor es necesariaa'] },
-});
-
-
->>>>>>> Arreglando Errores:server/models/CountBodega.js
 module.exports = mongoose.model('countBodega', countBodegaSchema);
